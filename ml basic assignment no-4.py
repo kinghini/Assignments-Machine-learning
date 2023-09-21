556b5fe283c16e65bcb01f1e48043bd1c0a04497
Nimmy Mathew <95744678+kinghini@users.noreply.github.com>
2023-09-21 20:49:30 +0530

    ml assignment no:4

---
+++ ml basic assignment no-4.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. What are the key tasks involved in getting ready to work with machine learning modeling?
+# 2. What are the different forms of data used in machine learning? Give a specific example for each of
+# them.
+# 3. Distinguish:
+# 
+# 1. Numeric vs. categorical attributes
+# 
+# 2. Feature selection vs. dimensionality reduction
+# 
+# 4. Make quick notes on any two of the following:
+# 
+# 1. The histogram
+# 
+# 2. Use a scatter plot
+# 
+# 3.PCA (Personal Computer Aid)
+# 
+# 5. Why is it necessary to investigate data? Is there a discrepancy in how qualitative and quantitative
+# data are explored?
+# 
+# 6. What are the various histogram shapes? What exactly are ‘bins&#39;?
+# 
+# 7. How do we deal with data outliers?
+# 
+# 8. What are the various central inclination measures? Why does mean vary too much from median in
+# certain data sets?
+# 
+# 9. Describe how a scatter plot can be used to investigate bivariate relationships. Is it possible to find
+# outliers using a scatter plot?
+# 
+# 10. Describe how cross-tabs can be used to figure out how two variables are related.
+
+# 1. **Machine Learning**:
+#    Machine learning is a subfield of artificial intelligence (AI) that focuses on the development of algorithms and statistical models that enable computer systems to improve their performance on a specific task through experience (data) without being explicitly programmed. Machine learning works by analyzing large datasets to discover patterns and relationships, and it uses this knowledge to make predictions or decisions. Two machine learning applications in the business world are:
+# 
+#    a. **Customer Churn Prediction**: Businesses can use machine learning to predict which customers are likely to leave (churn) based on their historical behavior and demographics. This helps companies take proactive measures to retain customers.
+# 
+#    b. **Recommendation Systems**: Online retailers and streaming services use machine learning to analyze user preferences and behavior to recommend products or content tailored to each user's interests, thereby increasing sales and user engagement.
+# 
+#    **Ethical Concerns**: Machine learning applications can raise ethical concerns related to bias in data, privacy violations, and unintended consequences. For instance, biased training data can lead to discriminatory outcomes, and data breaches can expose sensitive information.
+# 
+# 
+
+# 2. **Human Learning**:
+# 
+#    i. **Under the supervision of experts**: In this learning process, individuals acquire knowledge and skills under the guidance and mentorship of experts in a specific domain. This can occur through formal education, apprenticeships, or on-the-job training.
+# 
+#    ii. **With the assistance of experts in an indirect manner**: In this case, individuals may not have direct contact with experts but can access learning resources created by experts, such as books, online courses, or instructional videos.
+# 
+#    iii. **Self-education**: Self-education refers to individuals acquiring knowledge and skills independently, often through self-study, research, and experimentation, without formal guidance or expert assistance.
+# 
+# 
+
+# 3. **Types of Machine Learning**:
+#    - Supervised Learning
+#    - Unsupervised Learning
+#    - Reinforcement Learning
+#    - Semi-supervised Learning
+#    - Self-supervised Learning
+#    - Transfer Learning
+# 
+# 
+
+# 4. **Various Forms of Machine Learning**:
+#    Machine learning can be categorized based on the learning style and problem type, leading to various forms like supervised, unsupervised, reinforcement learning, etc.
+# 
+# 
+
+# 5. **Well-Posed Learning Problem**:
+#    A well-posed learning problem has the following characteristics:
+#    - **Clearly Defined Objectives**: The learning task's goals and success criteria are well-defined.
+#    - **Access to Relevant Data**: Sufficient and representative data is available for training and evaluation.
+#    - **Measurable Performance**: There are metrics to assess the model's performance.
+#    - **Feasibility**: The problem can be solved using the available algorithms and resources.
+# 
+# 
+
+# 6. **Machine Learning Capabilities**:
+#    Machine learning is a powerful tool for solving a wide range of problems, but it is not capable of solving all problems. It excels in tasks involving pattern recognition, but it may struggle with problems that lack sufficient data or well-defined objectives. Additionally, it may not be suitable for tasks requiring common sense reasoning or ethical judgment.
+# 
+# 
+
+# 7. **Methods and Technologies for Solving ML Problems**:
+#    Two methods are:
+#    - **Supervised Learning**: It uses labeled data to train models for tasks like classification and regression.
+#    - **Unsupervised Learning**: It explores data without labeled outcomes, often used for clustering and dimensionality reduction.
+# 
+# 
+
+# 8. **Forms of Supervised Learning**:
+#    - **Classification**: Predicts discrete labels (e.g., spam or not spam in email classification).
+#    - **Regression**: Predicts continuous values (e.g., predicting house prices based on features).
+# 
+# 
+
+# 9. **Difference Between Supervised and Unsupervised Learning**:
+# 
+#    - **Supervised Learning**: Requires labeled data for training and aims to predict predefined outcomes (e.g., classifying emails as spam or not spam).
+#    
+#    - **Unsupervised Learning**: Deals with unlabeled data and focuses on discovering hidden patterns or structures in data (e.g., clustering similar customer groups based on shopping behavior).
+# 
+# 
+
+# 10. **Machine Learning Process**:
+#     a. **Data Collection**: Gathering relevant data.
+#     b. **Data Preprocessing**: Cleaning, transforming, and preparing data.
+#     c. **Model Selection**: Choosing an appropriate machine learning algorithm.
+#     d. **Model Training**: Using training data to teach the model.
+#     e. **Model Evaluation**: Assessing the model's performance on test data.
+#     f. **Hyperparameter Tuning**: Optimizing model settings.
+#     g. **Deployment**: Integrating the model into a system or application.
+#     h. **Monitoring and Maintenance**: Continuously assessing and updating the model's performance.
+# 
+#     Notes on:
+#     - **MATLAB**: A high-level programming language often used for numerical computing and data analysis.
+#     - **Deep Learning Applications in Healthcare**: Deep learning techniques are applied in medical image analysis, disease diagnosis, drug discovery, and patient data analysis.
+#     - **Market Basket Analysis**: A data mining technique used in retail to identify patterns in customers' purchase behavior.
+#     - **Linear Regression (Simple)**: A basic regression technique used to model the relationship between a dependent variable and one independent variable.
+# 
+# 
+
+# 11. **Comparison**:
+# 
+#     1. **Generalization and Abstraction**: Generalization refers to the ability of a model to perform well on new, unseen data, while abstraction involves simplifying complex information into higher-level concepts.
+# 
+#     2. **Guided and Unsupervised Learning**: Guided learning, such as supervised learning, uses labeled data and guidance, while unsupervised learning operates on unlabeled data without specific guidance.
+# 
+#     3. **Regression and Classification**: Regression predicts continuous values (e.g., predicting house prices), while classification assigns discrete labels or categories (e.g., classifying emails as spam or not spam).
+
+# In[ ]:
+
+
+
+
